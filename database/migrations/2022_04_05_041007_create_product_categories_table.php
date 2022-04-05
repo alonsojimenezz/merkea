73048f3d2e81a5c9e0314f281841bc4821f8e40e
Alonso Jiménez <ajimenez@siccob.com.mx>
2022-04-05 09:57:05 -0500

    First Commit - Includes Access Control and first view for products categories

---
+++ database/migrations/2022_04_05_041007_create_product_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('Name')->unique();
+            $table->string('Description')->nullable();
+            $table->boolean('Active')->default(true);
+            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_categories');
+    }
+};
---
+++ database/seeders/ProductCategoriesSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use Faker\Factory as Faker;
+
+use App\Models\ProductCategories;
+
+class ProductCategoriesSeeder extends Seeder
+{
+    private $names = [];
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+
+        for ($i = 0; $i < 350; $i++) {
+
+            $name = $faker->unique()->word(1);
+            while ($this->isTaken($name)) {
+                $name = $faker->unique()->word(1);
+            }
+
+            ProductCategories::create([
+                'Name' => $name,
+                'Description' => $faker->sentence(10),
+            ]);
+        }
+    }
+
+    private function isTaken($name)
+    {
+        if (in_array($name, $this->names)) {
+            return true;
+        } else {
+            $this->names[] = $name;
+            return false;
+        }
+    }
+}
