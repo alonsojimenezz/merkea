8cc49d50ad9e017118f0d8ae02d2f05f6b5b19aa
Alonso Jiménez <ajimenez@siccob.com.mx>
2022-08-17 18:47:46 -0500

    Order orders in admin view

--- resources/views/components/javascript_module.blade.php
+++ resources/views/components/javascript_module.blade.php
@@ -1,3 +1,3 @@
 @isset($module)
-    <script type="module" src="{{ asset('assets/modules/' . $module) }}.js?v={{ date('YmdH') }}"></script>
+    <script type="module" src="{{ asset('assets/modules/' . $module) }}.js?v={{ date('Ymd') }}"></script>
 @endisset
